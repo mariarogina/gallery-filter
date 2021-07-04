bce50d4333d297278a1a75f55157070355cf91b2
Mariia <whitesheepa@gmail.com>
2021-07-04 17:00:23 +0300

    verstka

--- src/Projectlist.js
+++ src/Projectlist.js
@@ -10,7 +10,7 @@ export default class Projectlist extends Component {
      
     render() {
         const onelist = this.props.selected=="All" ? this.props.data  : this.props.data.filter((item) => (item.category == this.props.selected))
-		const list = onelist.map((item) => (<div className ="col-lg-3 col-md-6 mb-3"  key={item.img}  name = {item.category} > <img style={{width:'300px'}} className = "img-fluid mb-4" src ={item.img}/></div>))
+		const list = onelist.map((item) => (<div className ="col-lg-3 col-md-4 mb-2"  key={item.img}  name = {item.category} > <img style={{width:'400px'}} className = "img-fluid mb-4" src ={item.img}/></div>))
         
 
         return (
