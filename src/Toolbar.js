5311724137cc23230cc9b8aadbbe0ef77830ebef
Mariia <whitesheepa@gmail.com>
2021-07-07 12:52:19 +0300

    build

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,36 +1,29 @@
-import React, { Component } from 'react'
-import { uuid } from 'uuidv4';
+import React, { Component } from "react";
+import { uuid } from "uuidv4";
 
 export default class Toolbar extends Component {
+  render() {
 
-    
-    render() {
 
-        const active = this.props.selected === this.value;
-		const clazz = active ? 'btn-info' : 'btn-outline-secondary'
+    const buttons = this.props.filters.map((item, i) => {
 
-        const buttons = this.props.filters.map((item,i) => {
-         
-            return (
-                <button
-                
-                style={{margin:"10px"}} type='button'
-                className={`btn ${clazz}`}
-                 
-                    key={uuid()}
-                    value = {item}
-                    onClick={this.props.onSelectFilter}>
+        const active = this.props.selected === item;
+        const clazz = active ? "btn-info" : "btn-outline-secondary";
 
-                    {item}
-                    
-                    </button>
-            )
-        });
-        
-        return (
-            <div className="tool">
-                {buttons}
-            </div>
-        )
-    }
+      return (
+        <button
+          style={{ margin: "10px" }}
+          type="button"
+          className={`btn ${clazz}`}
+          key={uuid()}
+          value={item}
+          onClick={this.props.onSelectFilter}
+        >
+          {item}
+        </button>
+      );
+    });
+
+    return <div className="tool">{buttons}</div>;
+  }
 }
