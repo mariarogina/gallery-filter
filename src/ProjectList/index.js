1fb1f65b88531a08bb636f36ceb4ccd7dacd8f14
Mariia <whitesheepa@gmail.com>
2021-07-09 16:03:28 +0300

    function

--- src/ProjectList/index.js
+++ src/ProjectList/index.js
@@ -1,28 +1,84 @@
-import React, { Component } from "react";
+import React from "react";
 import uuid from "react-uuid";
 import PropTypes from "prop-types";
 
-export default class ProjectList extends Component {
-  render() {
-    const list = this.props.data.map((item) => (
-      <div className="col-lg-3 col-md-6 mb-2" key={uuid()} name={item.category}>
-        <img
-          style={{ width: "400px" }}
-          className="img-fluid mb-4"
-          src={item.img}
-          alt={item.category}
-        />
+export default function ProjectList({ data }) {
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <div className="row">
+        {data.map((item) => (
+          <div
+            className="col-lg-3 col-md-6 mb-2"
+            key={uuid()}
+            name={item.category}
+          >
+            <img
+              style={{ width: "400px" }}
+              className="img-fluid mb-4"
+              src={item.img}
+              alt={item.category}
+            />
+          </div>
+        ))}
       </div>
-    ));
-
-    return (
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        <div className="row">{list}</div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 ProjectList.propTypes = {
   data: PropTypes.array,
+};
+ProjectList.defaultProps = {
+  data: [{
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
+    category: "Flayers"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
+    category: "Flayers"
+  }],
 };
